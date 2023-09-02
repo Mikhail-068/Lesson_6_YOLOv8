bf5d43a7b3767e5206469acf72942099081f5f8d
Mikhail <mi.sidoren@gmail.com>
2023-09-02 20:14:37 +0400

    добавил все комментарии в MainActivity

--- app/src/main/java/com/example/yolov8/MainActivity.kt
+++ app/src/main/java/com/example/yolov8/MainActivity.kt
@@ -16,13 +16,25 @@ import java.util.*
 import java.util.concurrent.Executors
 
 class MainActivity : AppCompatActivity() {
+    // объект для работы с потоком видео с камеры на экране
     private lateinit var previewView: PreviewView
+
+    // объект для работы c кастомным view (RectView),
+    // который отображает рамку и текст вокруг объектов
     private lateinit var rectView: RectView
+
+    // объект инструмента, который необходим для работы с api-ONNX под JVM
     private lateinit var ortEnvironment: OrtEnvironment
+
+    // Объект для работы с инструментом-оберткой моделей ONNX.
+    // Позволяет нам работать с узлами модели
     private lateinit var session: OrtSession
 
+    // объект класса для работы с данными
+    // (подгружать модель НС, конвертировать форматы изображений и пр.)
     private val dataProcess = DataProcess(context = this)
 
+    // Код активации разрешения (используется в функции, которая активирует разрешения)
     companion object {
         const val PERMISSION = 1
     }
@@ -33,80 +45,105 @@ class MainActivity : AppCompatActivity() {
         previewView = findViewById(R.id.previewView)
         rectView = findViewById(R.id.rectView)
 
-        // 자동 꺼짐 해제
+        // отключаем авто-выключение экрана
+        // (по-умолчанию экран затемняется через некоторое время, мы это отключаем)
         window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
 
-        // 권한 허용
+        // вызываем функцию, которая проверит и, если оно не было запрошено ранее,
+        // запросит у пользователя разрешение на работу с камерой устройства
         setPermissions()
 
-        // onnx 파일 && txt 파일 불러오기
+        // загружаем и настраиваем модель
         load()
 
-        // 카메라 켜기
+        // настраиваем и включаем камеру
         setCamera()
     }
 
     private fun setCamera() {
-        //카메라 제공 객체
+        // получаем объект для работы с камерой устройства
         val processCameraProvider = ProcessCameraProvider.getInstance(this).get()
 
-        //전체 화면
+        // устанавливаем вывод картинки с камеры на полный экран
         previewView.scaleType = PreviewView.ScaleType.FILL_CENTER
 
-        // 전면 카메라
+        // необходимо выбрать камеру, с котрой будем работать (устанавливаем заднюю камеру)
         val cameraSelector =
             CameraSelector.Builder().requireLensFacing(CameraSelector.LENS_FACING_BACK).build()
 
-        // 16:9 화면으로 받아옴
+        // создаем объект для отображения картинки с камеры на экране
+        // настраиваем формат, в котором будет отображаться картинка с камеры (устанавливаем 16:9)
         val preview = Preview.Builder().setTargetAspectRatio(AspectRatio.RATIO_16_9).build()
 
-        // preview 에서 받아와서 previewView 에 보여준다.
+        // связываем созданный объект с объектом на нашем экране
         preview.setSurfaceProvider(previewView.surfaceProvider)
 
-        //분석 중이면 그 다음 화면이 대기중인 것이 아니라 계속 받아오는 화면으로 새로고침 함. 분석이 끝나면 그 최신 사진을 다시 분석
+        // создаем объект, который будет заниматься анализом кадров с камеры устройства
         val analysis = ImageAnalysis.Builder().setTargetAspectRatio(AspectRatio.RATIO_16_9)
             .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST).build()
 
+        // устанавливаем обработчик каждого кадра
         analysis.setAnalyzer(Executors.newSingleThreadExecutor()) {
+            // вызываем обработку кадра
             imageProcess(it)
+            // прекращаем работать с кадром
             it.close()
         }
 
-        // 카메라의 수명 주기를 메인 액티비티에 귀속
+        // привязываем процесс работы с камерой к жизненному циклу приложения,
+        // это необходимо, например, чтобы работа камеры и процесс обработки изображения
+        // были приостановлены, когда пользователь приостановил работу приложения (свернул его)
         processCameraProvider.bindToLifecycle(this, cameraSelector, preview, analysis)
     }
 
     private fun imageProcess(imageProxy: ImageProxy) {
+        // преобразуем кадр в формат bitmap
         val bitmap = dataProcess.imageToBitmap(imageProxy)
+        // преобразуем bitmap в floatBuffer для модели
         val floatBuffer = dataProcess.bitmapToFloatBuffer(bitmap)
-        val inputName = session.inputNames.iterator().next() // session 이름
-        //모델의 요구 입력값 [1 3 640 640] [배치 사이즈, 픽셀(RGB), 너비, 높이], 모델마다 크기는 다를 수 있음.
+        // ????
+        val inputName = session.inputNames.iterator().next()
+        //настраиваем входные данные для модели (в нашем случае [1 3 640 640])
         val shape = longArrayOf(
             DataProcess.BATCH_SIZE.toLong(),
             DataProcess.PIXEL_SIZE.toLong(),
             DataProcess.INPUT_SIZE.toLong(),
             DataProcess.INPUT_SIZE.toLong()
         )
+
+        // создаем тензор на основе кадра и заданных параметров
         val inputTensor = OnnxTensor.createTensor(ortEnvironment, floatBuffer, shape)
+
+        // запускаем обработку данных и получаем результат
         val resultTensor = session.run(Collections.singletonMap(inputName, inputTensor))
+
+        // берем первый элемент из массива результатов
         val outputs = resultTensor.get(0).value as Array<*> // [1 84 8400]
+
+        // преобразуем выходные данные модели
         val results = dataProcess.outputsToNPMSPredictions(outputs)
 
-        //화면 표출
+        // отображаем данные на экране (рисуем рамку и подписи)
         rectView.transformRect(results)
         rectView.invalidate()
     }
 
     private fun load() {
-        dataProcess.loadModel() // onnx 모델 불러오기
-        dataProcess.loadLabel() // coco txt 파일 불러오기
+        // подгружаем нашу модель
+        dataProcess.loadModel()
+        // подгружаем файл с сущностями для распознавания
+        dataProcess.loadLabel()
 
+        // получить объект для работы с ONNX
         ortEnvironment = OrtEnvironment.getEnvironment()
+        // создаем сессию ONNX
         session = ortEnvironment.createSession(
             this.filesDir.absolutePath.toString() + "/" + DataProcess.FILE_NAME,
             OrtSession.SessionOptions()
         )
 
+        // загружаем список распознаваемых сущностей во view,
+        // который на кадрах рисует границы и подписи
         rectView.setClassLabel(dataProcess.classes)
     }
 
@@ -116,9 +153,12 @@ class MainActivity : AppCompatActivity() {
         grantResults: IntArray
     ) {
         if (requestCode == PERMISSION) {
+            // проверяем установку разрешений после их программной активации
             grantResults.forEach {
+                // если они не установлены
                 if (it != PackageManager.PERMISSION_GRANTED) {
-                    Toast.makeText(this, "권한을 허용하지 않으면 사용할 수 없습니다", Toast.LENGTH_SHORT).show()
+                    // выводим предупреждение на экран
+                    Toast.makeText(this, "Проверьте разрешения для приложения!", Toast.LENGTH_SHORT).show()
                     finish()
                 }
             }
@@ -127,11 +167,15 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setPermissions() {
+        // считываем в лист разрешение из манифеста приложения
         val permissions = ArrayList<String>()
         permissions.add(android.Manifest.permission.CAMERA)
 
+        // двигаемся по всем элементам листа
         permissions.forEach {
+            // проверяем, есть ли разрешение от пользователя на использование того, что мы запрашивали
             if (ActivityCompat.checkSelfPermission(this, it) != PackageManager.PERMISSION_GRANTED) {
+                // если нет, то запрашиваем разрешение у пользователя
                 ActivityCompat.requestPermissions(this, permissions.toTypedArray(), PERMISSION)
             }
         }
